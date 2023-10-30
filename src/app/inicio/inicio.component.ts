425590dc3f22fcd3d23b7a1420021d39530fce18
Ivan Hnilicka <ivan.hnilicka21@outlook.com>
2023-10-29 23:14:23 -0600

    Paginas informativas terminadas. Se agrego hosting

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -9,7 +9,6 @@ import { DatabaseService } from '../database.service';
   styleUrls: ['./inicio.component.css']
 })
 
-
 export class InicioComponent implements OnInit, AfterViewInit {
   constructor(private bdService: DatabaseService) {}
 
@@ -21,7 +20,9 @@ export class InicioComponent implements OnInit, AfterViewInit {
     // Obtiene el link de descarga de las imagenes
     for(let i = 0; i < 5; i++){
       getDownloadURL(ref(storage, `noticias/noticia_${i + 1}.jpg`)).then(url => {
-        this.imagenes[i].src = url;
+        //this.imagenes[i].src = url;
+      }).catch((error) => {
+        console.log(error);
       })
     } 
   }
@@ -31,10 +32,6 @@ export class InicioComponent implements OnInit, AfterViewInit {
     document.querySelectorAll('.swiper-img').forEach(imagen => {
       this.carrusel.push(imagen as HTMLImageElement);
     });
-
-    this.carrusel[0].src = this.imagenes[0].src;
-    this.carrusel[1].src = this.imagenes[1].src;
-    this.carrusel[2].src = this.imagenes[2].src;
   }
 
   menuMovil = false;
@@ -49,39 +46,36 @@ export class InicioComponent implements OnInit, AfterViewInit {
   // Arreglo que guarda el link de las imagenes
   carrusel: HTMLImageElement[] = [];
   
-  // TODO: guardar imagenes en servidor para obtenerlas de ahi
   imagenes = [
     {
-      src: '',
+      src: 'https://firebasestorage.googleapis.com/v0/b/yale-school-of-art.appspot.com/o/noticias%2Fnoticia_1.jpg?alt=media&token=c2e08142-77e5-426d-a457-b291740a7d49',
       description: 'Visita la galería de Arte ubicada dentro de la Universidad'
     },
     {
-      src: '',
+      src: 'https://firebasestorage.googleapis.com/v0/b/yale-school-of-art.appspot.com/o/noticias%2Fnoticia_2.jpg?alt=media&token=b2bcf7a9-e77c-460e-b57f-cc082a027e4a',
       description: 'Te invitamos a la conferencia de Arte Moderno con Alejandro Vargas'
     },
     {
-      src: '',
+      src: 'https://firebasestorage.googleapis.com/v0/b/yale-school-of-art.appspot.com/o/noticias%2Fnoticia_3.jpg?alt=media&token=f0f9d8a1-2249-4ded-a5d5-2fd968bb6608',
       description: 'Publica tus obras en el foro oficial para compartir tu arte'
     },
     {
-      src: '',
+      src: 'https://firebasestorage.googleapis.com/v0/b/yale-school-of-art.appspot.com/o/noticias%2Fnoticia_4.jpg?alt=media&token=91151a39-10ea-4be0-93e0-6e4d774461f6',
       description: 'Universidad se prepara para cierre de ciclo escolar'
     },
     {
-      src: '',
+      src: 'https://firebasestorage.googleapis.com/v0/b/yale-school-of-art.appspot.com/o/noticias%2Fnoticia_5.jpg?alt=media&token=21741bce-1f0f-4655-b5ff-629b911e4f1a',
       description: 'Invita a tus amigos y familiares al curso de pintura para principiantes'
     },
   ]
 
   // Funciones para cambiar las imagenes hacia adelante y hacia atras respectivamente
   slideForward(): void {
-    let index = 0;
-    for(let i = 0; i < this.imagenes.length; i++){
-      if(this.imagenes[i].src === (this.carrusel[0].children.item(0) as HTMLImageElement).src){
-        index = i;
-        break;
-      }
-    }
+    let src = this.imagenes.map((img) => {
+      return img.src;
+    })
+
+    let index = src.indexOf((this.carrusel[0].children.item(0) as HTMLImageElement).src);
 
     for(let i = 0; i < this.carrusel.length; i++){
       (this.carrusel[i].children.item(0) as HTMLImageElement).src = this.imagenes[(i + index + 1) % this.imagenes.length].src;
@@ -91,13 +85,11 @@ export class InicioComponent implements OnInit, AfterViewInit {
   
 
   slideBack(): void {
-    let index = 0;
-    for(let i = 0; i < this.imagenes.length; i++){
-      if(this.imagenes[i].src === (this.carrusel[0].children.item(0) as HTMLImageElement).src){
-        index = i;
-        break;
-      }
-    }
+    let src = this.imagenes.map((img) => {
+      return img.src;
+    })
+
+    let index = src.indexOf((this.carrusel[0].children.item(0) as HTMLImageElement).src);
 
     for(let i = 0; i < this.carrusel.length; i++){
       if(index + (i - 1) < 0 ) {
